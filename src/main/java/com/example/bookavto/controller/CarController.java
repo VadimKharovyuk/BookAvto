10f6e7952f46e79a21736e01094dce5264ac8840
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2024-04-28 18:19:41 +0300

    fix button

--- src/main/java/com/example/bookavto/controller/CarController.java
+++ src/main/java/com/example/bookavto/controller/CarController.java
@@ -1,19 +1,16 @@
 package com.example.bookavto.controller;
 import com.example.bookavto.model.Car;
 import com.example.bookavto.service.CarService;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 @Controller
+@AllArgsConstructor
 public class CarController {
     private final CarService carService;
-
-    @Autowired
-    public CarController(CarService carService) {
-        this.carService = carService;
-    }
 
     @GetMapping("/cars")
     public String getAllCars(Model model) {
@@ -32,5 +29,11 @@ public class CarController {
     public String addCar(@ModelAttribute Car car) {
         carService.addCar(car);
         return "redirect:/cars";
+    }
+    // Метод для удаления машины
+    @PostMapping("/cars/delete")
+    public String deleteCar(@RequestParam("carId") Long carId) {
+        carService.deleteCar(carId);  // Удаление машины по ID
+        return "redirect:/cars";  // Перенаправление после удаления
     }
 }
